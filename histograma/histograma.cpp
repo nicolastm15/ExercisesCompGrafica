d829099ddb3b40c64bb05c6f4d143e2a1bebc86c
Nicolas <nicolasterramaia@gmail.com>
2020-10-15 16:41:06 -0300

    so pq meu linux ta dando problema

--- histograma/histograma.cpp
+++ histograma/histograma.cpp
@@ -7,16 +7,19 @@
 using namespace std;
 using namespace cv;
  
-vector<vector<int>> histograma(Mat i);
  
 int main(void) {
  Mat img = imread("a.jpeg");
  if (img.empty()){
      cerr << "Deu ruim";
  };
+ cout << img.cols;
 //  namedWindow("First Image", WINDOW_AUTOSIZE);
-imshow ("First Image", img);
-waitKey(0);
- 
- 
+for (int i; i<img.rows; i++){
+    for (int j; img.cols; i++){
+        vector<int> value_blue = img[i][j][0];
+        vector<int> value_green = img[i][j][1];
+        vector<int> value_red = img[i][j][2];
+    }
+}
 };
